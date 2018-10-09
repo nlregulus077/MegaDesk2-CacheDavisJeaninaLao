b8948f9460bcbe7005b7480bf10a3bb031f7a263
Jeanina Lao <lao13001@byui.edu>
2018-10-09 09:23:39 -0600

    MegaDesk 1.1

--- MegaDesk-3-JeaninaLao/ViewAllQuotes.Designer.cs
+++ MegaDesk-3-JeaninaLao/ViewAllQuotes.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             this.MainMenuButton = new System.Windows.Forms.Button();
+            this.gridQuotes = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.gridQuotes)).BeginInit();
             this.SuspendLayout();
             // 
             // MainMenuButton
             // 
-            this.MainMenuButton.Location = new System.Drawing.Point(12, 12);
+            this.MainMenuButton.Location = new System.Drawing.Point(316, 404);
             this.MainMenuButton.Name = "MainMenuButton";
             this.MainMenuButton.Size = new System.Drawing.Size(112, 35);
             this.MainMenuButton.TabIndex = 1;
@@ -41,15 +43,25 @@
             this.MainMenuButton.UseVisualStyleBackColor = true;
             this.MainMenuButton.Click += new System.EventHandler(this.MainMenuButton_Click);
             // 
+            // gridQuotes
+            // 
+            this.gridQuotes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridQuotes.Location = new System.Drawing.Point(12, 12);
+            this.gridQuotes.Name = "gridQuotes";
+            this.gridQuotes.Size = new System.Drawing.Size(709, 373);
+            this.gridQuotes.TabIndex = 2;
+            // 
             // ViewAllQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(734, 461);
+            this.Controls.Add(this.gridQuotes);
             this.Controls.Add(this.MainMenuButton);
             this.Name = "ViewAllQuotes";
             this.Text = "ViewAllQuotes";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ViewAllQuotes_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.gridQuotes)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -57,5 +69,6 @@
         #endregion
 
         private System.Windows.Forms.Button MainMenuButton;
+        private System.Windows.Forms.DataGridView gridQuotes;
     }
 }
