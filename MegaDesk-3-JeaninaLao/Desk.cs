8787280d98416241a36b62d9825274ec7a781ecc
davcache <dav14029@byui.edu>
2018-10-19 11:18:34 -0600

    Add and commented out struct in desk class

--- MegaDesk-3-JeaninaLao/Desk.cs
+++ MegaDesk-3-JeaninaLao/Desk.cs
@@ -21,4 +21,17 @@
         public SurfaceMaterial DeskMaterial { get; set; }
 
     }
+
+    //struct Desk1
+    //{
+    //    public int Width1;
+    //    public int Depth1;
+    //    public int NumberOfDrawers1;
+    //    public getInput(int Width1, int Depth1, int NumberOfDrawers1)
+    //    {
+    //        this.Width1 = Width1;
+    //        this.Depth1 = Depth1;
+    //        this.NumberOfDrawers1 = NumberOfDrawers1;
+    //    }
+    //}
 }
